f412c98eedbd58c38d88e99b959b43a3cba7e40a
Thaddeus Madison <thaddeus.madison@gmail.com>
2017-03-27 17:41:57 -0500

    moved states json file

--- src/pages/admin/create-event/create-event.ts
+++ src/pages/admin/create-event/create-event.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Observable, BehaviorSubject } from 'rxjs/Rx';
+import { states } from '../../../lib/json/states';
 
 @Component({
   templateUrl: 'create-event.html'
@@ -15,18 +16,9 @@ export class CreateEvent implements OnInit{
 
 
   ngOnInit(){
-    //this.http.get('json/states.json')
-    //        .map(res => {
-    //          console.log(res.json())
-    this.http.get('./assets/json/states.json').toPromise().then(res => {
-    this.states  = res.json();
-})
-           
-            .catch((error: any) => Observable.throw(error || 'Server error'));
-    
+    this.states = states;
+
   }
-
-
 
 }
 
